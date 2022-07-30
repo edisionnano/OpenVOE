76b9bd4c3fa2aa01b8f52f5aa832c267ef1ab156
spiros <samantas5855@gmail.com>
2022-07-30 19:00:49 +0300

    Fix bug with duplex sinks This bug could cause the callback to be called twice at the same loop if a device was of the duplex class

--- pipewire/device_change.cpp
+++ pipewire/device_change.cpp
@@ -17,16 +17,22 @@ static void registry_event_global(void *data, uint32_t id,
 	const string node_description = (temp = spa_dict_lookup(props, PW_KEY_NODE_DESCRIPTION)) ? string{temp} : "";
 	const string node_name = (temp = spa_dict_lookup(props, PW_KEY_NODE_NAME)) ? string{temp} : "";
 
+	bool deviceAdded{};
+
 	if (media_class == "Audio/Source" || media_class == "Audio/Source/Virtual" || media_class == "Audio/Duplex") {
 		audioInputDevices.push_back({node_description, node_name, id});
-		(*(ce.executor))(ce.callback, audioInputDevices, audioOutputDevices, videoInputDevices);
+		deviceAdded = true;
 	}
 	if (media_class == "Audio/Sink" || media_class == "Audio/Duplex") {
 		audioOutputDevices.push_back({node_description, node_name, id});
-		(*(ce.executor))(ce.callback, audioInputDevices, audioOutputDevices, videoInputDevices);
+		deviceAdded = true;
 	}
 	if (media_class == "Video/Source") {
 		videoInputDevices.push_back({node_description, node_name, id});
+		deviceAdded = true;
+	}
+
+	if (deviceAdded) {
 		(*(ce.executor))(ce.callback, audioInputDevices, audioOutputDevices, videoInputDevices);
 	}
 }
